e4e8a70d06440c6813027f91ec60b78d8226a43b
chichi <j43343@gmail.com>
2019-08-16 12:02:16 +0800

    driver.quit(); //關閉及釋放driver

--- src/main/java/idv/chi/webRobot/queryWeather_Chrome.java
+++ src/main/java/idv/chi/webRobot/queryWeather_Chrome.java
@@ -9,10 +9,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
- * Hello world!
+ * 需要打開瀏覽器(ex:Chrome)
  *
  */
-public class queryWeather 
+public class queryWeather_Chrome 
 {
 	private static WebDriver driver;
 	private static WebElement element;
@@ -28,17 +28,20 @@ public class queryWeather
 		jse = (JavascriptExecutor) driver;
 		driver.get("https://www.google.com/");// 開啟google
 		
-		driver.findElement(By.className("gLFyf")).sendKeys("中央氣象局");//搜尋輸入"中央氣象局" (ClassName找元素)
-		driver.findElement(By.name("btnK")).submit();//點擊"Google 搜尋" (name找元素)
+		driver.findElement(By.className("gLFyf")).sendKeys("中央氣象局");//搜尋輸入"中央氣象局" 
+		driver.findElement(By.name("btnK")).submit();//點擊"Google 搜尋" 
 
 		Thread.sleep(500);//延遲0.5秒
-		elements=driver.findElements(By.tagName("h3"));//搜尋出來的結果 (tagName找元素)
+		elements=driver.findElements(By.tagName("h3"));//搜尋出來的結果(標籤h3)
 		if(elements.size()>0){
 			elements.get(0).click();//點擊搜尋結果的第一個(天氣預報 - 中央氣象局)
 		}
 		
 		driver.findElement(By.linkText("國家公園")).click();//點擊"國家公園"連結
 		driver.findElement(By.partialLinkText("墾丁")).click();//點擊"含""墾丁"連結(欲查詢墾丁國家公園)
+		
+		Thread.sleep(3000);
+		driver.quit(); //關閉及釋放driver
 				
 	}
 
